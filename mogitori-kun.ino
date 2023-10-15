3796da64c86cbad897855cf3e8619bb7a384c327
emp-temp <emptemp0000@gmail.com>
2023-10-15 17:55:50 +0900

    refactor: hoge

--- mogitori-kun.ino
+++ mogitori-kun.ino
@@ -7,22 +7,40 @@
 #endif
 #include <SPI.h>
 
-// Function prototype
-void moveMecanum(int, int, int);
-void driveMotor(int, int, int);
-float degreeToRadian(int);
-
 USB Usb;
+//USBHub Hub1(&Usb); // Some dongles have a hub inside
+BTD Btd(&Usb); // You have to create the Bluetooth Dongle instance like so
 
-BTD Btd(&Usb); 
 /* You can create the instance of the PS4BT class in two ways */
 // This will start an inquiry and then pair with the PS4 controller - you only have to do this once
 // You will need to hold down the PS and Share button at the same time, the PS4 controller will then start to blink rapidly indicating that it is in pairing mode
-PS4BT PS4(&Btd, PAIR);
+//PS4BT PS4(&Btd, PAIR);
 
 // After that you can simply create the instance like so and then press the PS button on the device
-//PS4BT PS4(&Btd);
+PS4BT PS4(&Btd);
 
+
+void setup() {
+  Serial.begin(115200);
+#if !defined(__MIPSEL__)
+  while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
+#endif
+  if (Usb.Init() == -1) {
+    Serial.print(F("\r\nOSC did not start"));
+    while (1); // Halt
+  }
+  Serial.print(F("\r\nPS4 Bluetooth Library Started"));
+}
+
+struct Motor {
+  int dir_pin;
+  int pwm_pin;
+};
+
+struct Relay {
+  int pin;
+  bool is_high;
+};
 
 // WARNNING: Specify here the motor to be used for mecanum
 /*
@@ -35,128 +53,20 @@ PS4BT PS4(&Btd, PAIR);
 */
 // WARNNING: Connect A to the red conductor
 // WARNNING: Connect B to the black conductor
-int motor1_dir = 29;
-int motor1_pwm = 9;
-int motor2_dir = 37;
-int motor2_pwm = 10;
-int motor3_dir = 28;
-int motor3_pwm = 8;
-int motor4_dir = 35;
-int motor4_pwm = 6;
-int motor5_dir = 36;
-int motor5_pwm = 4;
-int motor6_dir = 39;
-int motor6_pwm = 5;
+Motor front_left_motor = {29, 9};
+Motor front_right_motor = {37, 10};
+Motor center_left_motor = {28, 8};
+Motor center_right_motor = {35, 6};
+Motor rear_left_motor = {36, 4};
+Motor rear_right_motor = {39, 5};
 
-int arm_motor_pair1_dir = 48;
-int arm_motor_pair1_pwm = 46;
-int arm_motor_pair2_dir = 47;
-int arm_motor_pair2_pwm = 44;
-int arm_motor_pair3_dir = 34;
-int arm_motor_pair3_pwm = 45;
+Motor arm_pair1 = {48, 46};
+Motor arm_pair2 = {47, 44};
+Motor arm_pair3 = {34, 45};
 
-
-int relay12 = 24;
-bool relay12_is_high = false;
-int relay34 = 26;
-bool relay34_is_high = false;
-int relay56 = 33;
-bool relay56_is_high = false;
-
-void setup() {
-  Serial.begin(115200);
-#if !defined(__MIPSEL__)
-  while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
-#endif
-  if (Usb.Init() == -1) {
-    Serial.print(F("\r\nOSC did not start"));
-    while (1); // Halt
-  }
-  Serial.print(F("\r\nPS4 Bluetooth Library Started"));
-
-  // motor 1
-  pinMode(motor1_dir, OUTPUT);
-  pinMode(motor1_pwm, OUTPUT);
-  // motor 2
-  pinMode(motor2_dir, OUTPUT);
-  pinMode(motor2_pwm, OUTPUT);
-  // motor 3
-  pinMode(motor3_dir, OUTPUT);
-  pinMode(motor4_pwm, OUTPUT);
-  // motor 4
-  pinMode(motor4_dir, OUTPUT);
-  pinMode(motor4_pwm, OUTPUT);
-
-  // arm_motor_pair1
-  pinMode(arm_motor_pair1_dir, OUTPUT);
-  pinMode(arm_motor_pair1_pwm, OUTPUT);
-  // arm_motor_pair2
-  pinMode(arm_motor_pair2_dir, OUTPUT);
-  pinMode(arm_motor_pair2_pwm, OUTPUT);
-  // arm_motor_pair3
-  pinMode(arm_motor_pair3_dir, OUTPUT);
-  pinMode(arm_motor_pair3_pwm, OUTPUT);
-
-  // relay12
-  pinMode(relay12, OUTPUT);
-  // relay34
-  pinMode(relay34, OUTPUT);
-  // relay56
-  pinMode(relay56, OUTPUT);
-}
-
-
-
-void loop() {
-  Usb.Task();
-
-  while (true) {
-    if (PS4.connected()) {
-      // Receive controller input
-      int Vx = map(PS4.getAnalogHat(LeftHatX), 0, 255, -255, 255);
-      int Vy = map(PS4.getAnalogHat(LeftHatY), 0, 255, -255, 255);
-      int Vr = map(PS4.getAnalogHat(RightHatX), 0, 255, -255, 255);
-
-      if (PS4.getButtonClick(CROSS)) {
-        relayToggle(relay12, &relay12_is_high);
-      }
-      if (PS4.getButtonClick(SQUARE)) {
-        relayToggle(relay34, &relay34_is_high);
-      }
-      if (PS4.getButtonClick(CIRCLE)) {
-        relayToggle(relay56, &relay56_is_high);
-      }
-
-      if (PS4.getButtonPress(UP)) {
-        driveMotor(arm_motor_pair1_dir, arm_motor_pair1_pwm, 180);
-      } 
-      if (PS4.getButtonPress(DOWN)) {
-        driveMotor(arm_motor_pair1_dir, arm_motor_pair1_pwm, -180);
-      } 
-      if (PS4.getButtonPress(R1)) {
-        driveMotor(arm_motor_pair3_dir, arm_motor_pair3_pwm, 180);
-      }
-      if (PS4.getAnalogButton(R2) < 180) {
-        driveMotor(arm_motor_pair1_dir, arm_motor_pair1_pwm, -180);
-      }
-      if (PS4.getButtonPress(L1)) {
-        driveMotor(arm_motor_pair2_dir, arm_motor_pair2_pwm, 180);
-      }
-      if (PS4.getAnalogButton(L2) < 180) {
-        driveMotor(arm_motor_pair2_dir, arm_motor_pair2_pwm, -180);
-      }
-
-      // If the input is too small, ignore it(Input range is -30 to 30)
-      if (abs(Vx) < 30 && abs(Vy) < 30 && abs(Vr) < 30) {
-        continue;
-      }
-
-      // Control the mecanum using the received coordinate values
-      moveMecanum(Vx, Vy, Vr);
-    }
-  }
-}
-
+Relay front_air_cylinder = {24, true};
+Relay center_air_cylinder = {26, true};
+Relay rear_air_cylinder = {33, true};
 
 
 // Control mecanum from coordinate values
@@ -180,12 +90,12 @@ void moveMecanum(int Vx, int Vy, int Vr) {
     wheel5 = constrain(wheel5, -255, 255);
     wheel6 = constrain(wheel6, -255, 255);
 
-    driveMotor(motor1_dir, motor1_pwm, wheel1);
-    driveMotor(motor2_dir, motor2_pwm, wheel2);
-    driveMotor(motor3_dir, motor3_pwm, wheel3);
-    driveMotor(motor4_dir, motor4_pwm, wheel4);
-    driveMotor(motor5_dir, motor5_pwm, wheel5);
-    driveMotor(motor6_dir, motor6_pwm, wheel6);
+    driveMotor(&front_left_motor, wheel1);
+    driveMotor(&front_right_motor, wheel2);
+    driveMotor(&center_left_motor, wheel3);
+    driveMotor(&center_right_motor, wheel4);
+    driveMotor(&rear_left_motor, wheel5);
+    driveMotor(&rear_right_motor, wheel6);
   } else {
     // Calculate for each mecanum wheel
     wheel1 = Vy + Vx;  // left front
@@ -216,12 +126,12 @@ void moveMecanum(int Vx, int Vy, int Vr) {
     wheel6 = constrain(wheel6, -255, 255);
 
     // Controls each motor
-    driveMotor(motor1_dir, motor1_pwm, wheel1);
-    driveMotor(motor2_dir, motor2_pwm, wheel2);
-    driveMotor(motor3_dir, motor3_pwm, wheel3);
-    driveMotor(motor4_dir, motor4_pwm, wheel4);
-    driveMotor(motor5_dir, motor5_pwm, wheel5);
-    driveMotor(motor6_dir, motor6_pwm, wheel6);
+    driveMotor(&front_left_motor, wheel1);
+    driveMotor(&front_right_motor, wheel2);
+    driveMotor(&center_left_motor, wheel3);
+    driveMotor(&center_right_motor, wheel4);
+    driveMotor(&rear_left_motor, wheel5);
+    driveMotor(&rear_right_motor, wheel6);
   }
 }
 
@@ -235,24 +145,117 @@ float degreeToRadian(int degree) {
 
 
 // Control motors (0~255)
-void driveMotor(int pin_dir, int pin_pwm, int speed) {
+void driveMotor(struct Motor* motor, int speed) {
   if (speed > 0) {
-    digitalWrite(pin_dir, LOW);
-    analogWrite(pin_pwm, speed);
+    digitalWrite(motor->dir_pin, LOW);
+    analogWrite(motor->pwm_pin, speed);
   } else {
-    digitalWrite(pin_dir, HIGH);
-    analogWrite(pin_pwm, speed);
+    digitalWrite(motor->dir_pin, HIGH);
+    analogWrite(motor->pwm_pin, speed);
   }
 }
 
 
 // Control air cylinders
-void relayToggle(int pin_relay, bool* is_high) {
-  if (is_high) {
-    digitalWrite(pin_relay, LOW);
-    *is_high = false;
+void relayToggle(struct Relay* relay) {
+  if (relay->is_high) {
+    digitalWrite(relay->pin, LOW);
+    relay->is_high = false;
   } else {
-    digitalWrite(pin_relay, HIGH);
-    *is_high = true;
+    digitalWrite(relay->pin, HIGH);
+    relay->is_high = true;
   }
 }
+
+void init_pins() {
+  // motor 1
+  pinMode(front_left_motor.dir_pin, OUTPUT);
+  pinMode(front_left_motor.pwm_pin, OUTPUT);
+  // motor 2
+  pinMode(front_right_motor.dir_pin, OUTPUT);
+  pinMode(front_right_motor.pwm_pin, OUTPUT);
+  // motor 3
+  pinMode(center_left_motor.dir_pin, OUTPUT);
+  pinMode(center_left_motor.pwm_pin, OUTPUT);
+  // motor 4
+  pinMode(center_right_motor.dir_pin, OUTPUT);
+  pinMode(center_right_motor.pwm_pin, OUTPUT);
+
+  // arm_motor_pair1
+  pinMode(arm_pair1.dir_pin, OUTPUT);
+  pinMode(arm_pair1.pwm_pin, OUTPUT);
+  // arm_motor_pair2
+  pinMode(arm_pair2.dir_pin, OUTPUT);
+  pinMode(arm_pair2.pwm_pin, OUTPUT);
+  // arm_motor_pair3
+  pinMode(arm_pair3.dir_pin, OUTPUT);
+  pinMode(arm_pair3.pwm_pin, OUTPUT);
+
+  pinMode(front_air_cylinder.pin, OUTPUT);
+  pinMode(center_air_cylinder.pin, OUTPUT);
+  pinMode(rear_air_cylinder.pin, OUTPUT);
+}
+
+void loop() {
+  Usb.Task();
+
+  init_pins();
+
+  while (true) {
+    if (PS4.connected()) {
+      // Receive controller input
+      int Vx = map(PS4.getAnalogHat(RightHatX), 0, 255, -255, 255);
+      int Vy = map(PS4.getAnalogHat(RightHatY), 0, 255, -255, 255);
+      // int Vr = map(PS4.getAnalogHat(RightHatX), 0, 255, -255, 255);:
+      int Vr  = 0;
+
+      if (PS4.getButtonClick(CROSS)) {
+        relayToggle(&front_air_cylinder);
+        Serial.print(F("\r\nCROSS"));
+      }
+      if (PS4.getButtonClick(SQUARE)) {
+        relayToggle(&center_air_cylinder);
+        Serial.print(F("\r\nSQUARE"));
+      }
+      if (PS4.getButtonClick(CIRCLE)) {
+        relayToggle(&rear_air_cylinder);
+        Serial.print(F("\r\nCIRCLE"));
+      }
+
+
+      if (PS4.getButtonPress(UP)) {
+        driveMotor(&arm_pair1, 180);
+        Serial.print(F("\r\nUP"));
+      } 
+      if (PS4.getButtonPress(DOWN)) {
+        driveMotor(&arm_pair1, -180);
+        Serial.print(F("\r\nDOWN"));
+      } 
+      if (PS4.getButtonPress(R1)) {
+        driveMotor(&arm_pair2, 180);
+        Serial.print(F("\r\nR1"));
+      }
+      if (PS4.getAnalogButton(R2) < 180) {
+        driveMotor(&arm_pair2, -180);
+        Serial.print(F("\r\nR2"));
+      }
+      if (PS4.getButtonPress(L1)) {
+        driveMotor(&arm_pair3, 180);
+        Serial.print(F("\r\nL1"));
+      }
+      if (PS4.getAnalogButton(L2) < 180) {
+        driveMotor(&arm_pair3, -180);
+        Serial.print(F("\r\nL2"));
+      }
+
+      // If the input is too small, ignore it(Input range is -30 to 30)
+      if (abs(Vx) < 30 && abs(Vy) < 30 && abs(Vr) < 30) {
+        continue;
+      }
+
+      // Control the mecanum using the received coordinate values
+      moveMecanum(Vx, Vy, Vr);
+    }
+  }
+}
+
